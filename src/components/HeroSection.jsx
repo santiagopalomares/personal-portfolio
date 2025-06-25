f48986cb5afb6db902aebb6793b63cbf7bf07335
santiago <spaloma.v1@gmail.com>
2025-06-24 19:29:21 -0700

    nit: perfect padding

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export const HeroSection = () => {
   return (
     <section
       id="hero"
-      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4 pt-28 md:pt-32"
     >
       <div className="container max-w-4xl mx-auto text-center z-10">
         <div className="space-y-6">
